736a4d369f0b9b322104d0f14eb8f0a93a6a1fb0
Stanislav Pugachev <129574544+stanislav-pugachev@users.noreply.github.com>
2023-04-06 05:24:28 +0100

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,109 @@
+basic.forever(function () {
+    basic.pause(200)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # .
+        # # # # .
+        # # # # #
+        . # # # #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # .
+        # # # # .
+        . # # # .
+        . # # # #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # # .
+        . . # # .
+        . # # # .
+        . # # . .
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # # .
+        . . # # .
+        . . # . .
+        . # # . .
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+})
